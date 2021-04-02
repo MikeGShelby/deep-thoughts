1cb20da01df06681bb99e4c9854081531f39b8d3
Mike Shelby <mikegshelby@gmail.com>
2021-04-02 14:27:23 -0500

    added ThoughtForm component to Home and Profile pages.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Auth from '../utils/auth';
+
+import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
+import FriendList from '../components/FriendList';
+
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList'
-import Auth from '../utils/auth';
+
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -19,6 +23,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+          {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
