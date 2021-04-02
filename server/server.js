d32f36e074bc7d8db28c26556a1197a5308c74d7
Mike Shelby <mikegshelby@gmail.com>
2021-04-02 17:43:44 -0500

    changed db connection back to db.once()

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Removing this to determine if this fixes Heroku problems
-// db.once('open', () => {
-
-db.open('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
